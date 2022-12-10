5915d5d09e59c7760f1d65b167b378e703bd9da8
Roland Braun <dev@rolandbraun.com>
2022-12-10 10:13:09 +0100

    Update Added documentation.

--- Classes/ConvertFromJSON.cs
+++ Classes/ConvertFromJSON.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace IfKThenX;
 
+/// <summary>
+/// Provides usefull methods to convert from JSON to objects used in IfKThenX.
+/// </summary>
 public class ConvertFromJSON
 {
+    /// <summary>
+    /// Converts from an JSON file to a list of <see cref="IState"/> objects.
+    /// </summary>
+    /// <param name="filePath">The absolute path to the JSON file.</param>
+    /// <returns>A <see cref="Task"/> representing the process of generating the list of <see cref="IState"/>.</returns>
     public static async Task<List<IState>> ToIStatesAsync(string filePath)
     {
         using FileStream openStream = File.OpenRead(filePath);
@@ -19,6 +27,11 @@ public class ConvertFromJSON
         return new List<IState>(parsedStates);
     }
 
+    /// <summary>
+    /// Converts from an JSON file to an <see cref="IBusState"/>.
+    /// </summary>
+    /// <param name="filePath">The absolute path to the JSON file.</param>
+    /// <returns>A <see cref="Task"/> representing the process of generating the <see cref="IBusState"/>.</returns>
     public static async Task<IBusState> ToIBusStateAsync(string filePath)
     {
         List<IState> states = await ToIStatesAsync(filePath);
